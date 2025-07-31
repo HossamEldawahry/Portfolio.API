86f0a1c6156f9d1f54060c39c0299fa181bbfeef
Hossam Mostafa <hossamarab2024@gmail.com>
2025-07-31 11:52:18 +0300

    0

--- Portfolio.API/Program.cs
+++ Portfolio.API/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     });
     options.AddPolicy("localPolicy", policy =>
     {
-        policy.WithOrigins("https://localhost:4200") // Angular dev server
+        policy.WithOrigins("http://localhost:4200/") // Angular dev server
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
